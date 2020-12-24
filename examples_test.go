b6749586aa096a54fec3ea635ee7098dc248315c
Meromen <y.pysin@corp.mail.ru>
2020-12-24 17:29:17 +1000

    Add examples

---
+++ examples_test.go
@@ -0,0 +1,52 @@
+package text_cleaner
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func ExampleClean() {
+	res, err := http.Get("https://google.com")
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	cfg := WhiteListConfig{
+		Eng:   true,
+		Rus:   true,
+		Dig:   true,
+		AddWl: "",
+	}
+
+	result := Clean(res.Body, cfg)
+	fmt.Println(result)
+}
+
+func ExampleCleanBytes() {
+	sb := []byte("123 asd   фыв   ")
+
+	cfg := WhiteListConfig{
+		Eng:   true,
+		Rus:   true,
+		Dig:   false,
+		AddWl: "",
+	}
+
+	result := CleanBytes(sb, cfg)
+	fmt.Println(result) // "asd фыв"
+}
+
+func ExampleCleanString() {
+	str := "Hello it's    fast cleaner очень быстрый, быстрее 10 самолетов....    "
+
+	cfg := WhiteListConfig{
+		Eng:   true,
+		Rus:   false,
+		Dig:   true,
+		AddWl: "'т",
+	}
+
+	result := CleanString(str, cfg)
+	fmt.Println(result) // "Hello it's fast cleaner т т 10 т"
+}
